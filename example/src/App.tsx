f35f1c3745163fc8d94369e229d8301b5737ed4f
Mark Lawlor <mwlawlor@gmail.com>
2025-10-13 09:46:37 +1000

    fix: types

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,13 @@
 import { View, StyleSheet } from 'react-native';
 import { multiply, StyleRegistry } from 'react-native-css-nitro';
-import Text from 'react-native-css-nitro/components/Text';
+import { Text } from 'react-native-css-nitro/components/Text';
 
 StyleRegistry.set('text-red-500', { s: [], d: [{ color: 'red' }] });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Multiply: {multiply(3, 7)}</Text>
+      <Text className="text-red-500">Multiply: {multiply(3, 7)}</Text>
     </View>
   );
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,1 +1,6 @@
-export { Text } from 'react-native';
+import { Text as RNText, type TextProps } from 'react-native';
+import { copyComponentProperties } from '../../utils';
+
+export const Text = copyComponentProperties(RNText, (props: TextProps) => {
+  return <RNText {...props} />;
+});
